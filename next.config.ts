32820d8c6a725ec4243b51c7cfd7f3441bc6a53b
IvanCardi <ivancardillo1996@gmail.com>
2025-04-28 11:53:57 +0200

    fix: adds remote patterns for image

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/uploads/:path*",
+        destination: "http://192.168.1.102:3000/:path*",
+      },
+    ];
+  },
   images: {
     remotePatterns: [new URL("http://localhost:3000/**")],
   },
